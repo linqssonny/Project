448cc97778a72cf994d4437d90b17a3facea63f4
linqingsong <251593901@qq.com>
2017-09-13 21:00:59 +0800

    增加常用工具类

---
+++ utilslibs/src/main/java/com/library/utils/code/UrlCodeUtils.java
@@ -0,0 +1,53 @@
+package com.library.utils.code;
+
+import android.text.TextUtils;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * Created by linqs on 2017/4/18.
+ */
+
+public class UrlCodeUtils {
+
+    /**
+     * UTF-8方式encode字符串
+     *
+     * @param value
+     * @return
+     */
+    public static String encodeByUTF8(String value) {
+        if (TextUtils.isEmpty(value)) {
+            return value;
+        }
+        String returnValue;
+        try {
+            String s = URLEncoder.encode(value, "UTF-8");
+            returnValue = s;
+        } catch (Exception e) {
+            returnValue = value;
+        }
+        return returnValue;
+    }
+
+    /**
+     * UTF-8方式decode字符串
+     *
+     * @param value
+     * @return
+     */
+    public static String decodeByUTF8(String value) {
+        if (TextUtils.isEmpty(value)) {
+            return value;
+        }
+        String returnValue;
+        try {
+            String s = URLDecoder.decode(value, "UTF-8");
+            returnValue = s;
+        } catch (Exception e) {
+            returnValue = value;
+        }
+        return returnValue;
+    }
+}
